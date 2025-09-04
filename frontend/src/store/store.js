d44a5a68485037f4e327745f0e40d7c42df36863
Dhiraj Kumar <dhirajkumar62413@gmail.com>
2025-09-04 18:13:31 +0530

    commit

---
+++ frontend/src/store/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import usersReducer from "../redux/slices/users/userSlices";
+
+//!Store
+const store = configureStore({
+    reducer: {
+        users: usersReducer,
+    }
+})
